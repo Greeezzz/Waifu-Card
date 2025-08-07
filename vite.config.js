942fbb8044416ca409a3965904273fb3c6039a4e
greed <greed@192.168.1.90>
2025-08-07 15:34:05 +0700

    third

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base : '/Waifu-Card/', // Set the base path for GitHub Pages
   plugins: [react()],
+  base: '/Waifu-Card/' // pastikan sama dengan nama repo
 })
